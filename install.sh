75bfb87704fe769171202b9a71c6a688a135df36
richard-ys-lin <richard_ys_lin@trendmicro.com>
2024-05-21 23:34:14 +0800

    install: install lua-format & mason tools

--- install.sh
+++ install.sh
@@ -27,19 +27,17 @@ git clone git@github.com:ZhuMon/nvimrc.git "$nvim_dir"
 # Install packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+# Install lua-format in the background
+luarocks install --server=https://luarocks.org/dev luaformatter &
+
 # Install plugins by packer
 nvim -c 'PackerInstall'
+
+# Install Mason tools
+nvim -c 'MasonToolsUpdate'
+
 
 # Install the nerd font
 curl -L -o /tmp/BitstreamVeraSansMono.zip --url "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/BitstreamVeraSansMono.zip"
 unzip /tmp/BitstreamVeraSansMono.zip -d /tmp/BitstreamVeraSansMono
 cp /tmp/BitstreamVeraSansMono/*.ttf ~/Library/Fonts/
-
-# Install copilot
-if [ ! -d "$nvim_dir/pack/github/start" ]; then
-    mkdir -p "$nvim_dir/pack/github/start"
-    git clone https://github.com/github/copilot.vim.git \
-        ~/.config/nvim/pack/github/start/copilot.vim
-else
-    echo "copilot.vim already exists"
-fi
