0b376a508e97b5eb98b3b5a57479320763237de3
richard-ys-lin <richard_ys_lin@trendmicro.com>
2024-10-25 12:17:21 +0800

    add jk keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.api.nvim_set_keymap
+-- ## Insert mode
+map("i", "jk", "<Esc>", { noremap = true, silent = true })
+map("i", "Jk", "<Esc>", { noremap = true, silent = true })
+map("i", "jK", "<Esc>", { noremap = true, silent = true })
+map("i", "JK", "<Esc>", { noremap = true, silent = true })
