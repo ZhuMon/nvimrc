65d5719a5819f8752d0e0d21897a96d154614581
ZhuMon <richardlin0212@gmail.com>
2024-12-19 11:47:19 +0800

    plugins(ai): add copliotchat setting

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,25 @@
+return {
+  --   {
+  --     "CopilotC-Nvim/CopilotChat.nvim",
+  --     opts = function()
+  --       local user = vim.env.USER or "User"
+  --       user = user:sub(1, 1):upper() .. user:sub(2)
+  --       return {
+  --         auto_insert_mode = true,
+  --         question_header = "  " .. user .. " ",
+  --         answer_header = "  Copilot ",
+  --         window = {
+  --           width = 0.4,
+  --         },
+  --         prompts = {
+  --           Yarr = {
+  --             prompt = "Explain how it works.",
+  --             system_prompt = "You are very good at explaining stuff",
+  --             -- mapping = "<leader>ccmc",
+  --             description = "My custom prompt description",
+  --           },
+  --         },
+  --       }
+  --     end,
+  --   },
+}
